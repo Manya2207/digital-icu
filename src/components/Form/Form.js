d1272584cf10c58a355b91cf5b1dcddfcc0d40b9
Manya Sharma <sharma22manya@gmail.com>
2023-08-08 20:28:29 +0530

    first commit

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -57,166 +57,177 @@ const Form = () => {
     <div className="form-container">
       Hospital Details
       {sections.map((section, sectionIndex) => (
-        <form className="form" key={sectionIndex} onSubmit={handleSubmit}>
-          {/* Hospital Details */}
-          <InputGroup columns={1}>
-            <CustomInput
-              label="Hospital Name"
-              name="hospitalName"
-              type="text"
-              value={section.hospitalName}
-              onChange={(e) =>
-                handleChange(sectionIndex, 0, "hospitalName", e.target.value)
-              }
-            />
-          </InputGroup>
-
-          <InputGroup columns={3}>
-            <CustomInput
-              label="Hospital Pincode"
-              name="hospitalPincode"
-              type="dropdown"
-              value={section.hospitalPincode}
-              options={genderOptions}
-              onChange={(e) =>
-                handleChange(sectionIndex, 0, "hospitalPincode", e.target.value)
-              }
-            />
-            <CustomInput
-              label="Hospital State"
-              name="hospitalState"
-              type="text"
-              value={section.hospitalState}
-              onChange={(e) =>
-                handleChange(sectionIndex, 0, "hospitalState", e.target.value)
-              }
-            />
-            <CustomInput
-              label="Hospital City"
-              name="hospitalCity"
-              type="text"
-              value={section.hospitalCity}
-              onChange={(e) =>
-                handleChange(sectionIndex, 0, "hospitalCity", e.target.value)
-              }
-            />
-          </InputGroup>
-
-          <InputGroup columns={1}>
-            <CustomInput
-              label="ICU Name"
-              name="icuName"
-              type="text"
-              value={section.icuName}
-              onChange={(e) =>
-                handleChange(sectionIndex, 0, "icuName", e.target.value)
-              }
-            />
-          </InputGroup>
-
-          <InputGroup columns={4}>
-            <CustomInput
-              label="Camera IP Address"
-              name="cameraIpAddress"
-              type="text"
-              value={section.cameraIpAddress}
-              onChange={(e) =>
-                handleChange(sectionIndex, 0, "cameraIpAddress", e.target.value)
-              }
-            />
-            <CustomInput
-              label="RTSP Port Number"
-              name="rtspPort"
-              type="text"
-              value={section.rtspPort}
-              onChange={(e) =>
-                handleChange(sectionIndex, 0, "rtspPort", e.target.value)
-              }
-            />
-            <CustomInput
-              label="Username"
-              name="username"
-              type="text"
-              value={section.username}
-              onChange={(e) =>
-                handleChange(sectionIndex, 0, "username", e.target.value)
-              }
-            />
-            <CustomInput
-              label="Password"
-              name="password"
-              type="text"
-              value={section.password}
-              onChange={(e) =>
-                handleChange(sectionIndex, 0, "password", e.target.value)
-              }
-            />
-          </InputGroup>
-
-          {/* Bed Section */}
-          <h3>Bed Details</h3>
-          {section.beds.map((bed, bedIndex) => (
-            <InputGroup columns={3} key={bedIndex}>
+        <>
+          <form className="form" key={sectionIndex} onSubmit={handleSubmit}>
+            {/* Hospital Details */}
+            <InputGroup columns={1}>
               <CustomInput
-                label={`${bed.bedNumber}`}
-                name={`bed-${bed.bedNumber}`}
+                label="Hospital Name"
+                name="hospitalName"
                 type="text"
-                value={bed.monitorIP}
+                value={section.hospitalName}
                 onChange={(e) =>
-                  handleChange(
-                    sectionIndex,
-                    bedIndex,
-                    "monitorIP",
-                    e.target.value
-                  )
-                }
-              />
-              <CustomInput
-                label="Upper Body Preset Number"
-                name={`upper-body-preset-${bed.bedNumber}`}
-                type="text"
-                value={bed.upperBodyPreset}
-                onChange={(e) =>
-                  handleChange(
-                    sectionIndex,
-                    bedIndex,
-                    "upperBodyPreset",
-                    e.target.value
-                  )
-                }
-              />
-              <CustomInput
-                label="Lower Body Preset Number"
-                name={`lower-body-preset-${bed.bedNumber}`}
-                type="text"
-                value={bed.lowerBodyPreset}
-                onChange={(e) =>
-                  handleChange(
-                    sectionIndex,
-                    bedIndex,
-                    "lowerBodyPreset",
-                    e.target.value
-                  )
+                  handleChange(sectionIndex, 0, "hospitalName", e.target.value)
                 }
               />
             </InputGroup>
-          ))}
 
-          <button
-            className="bg-orange-500 text-white font-bold py-2 px-4 rounded-lg focus:outline-none hover:bg-blue-600"
-            type="button"
-            onClick={() => handleAddBed(sectionIndex)}
-          >
-            + Add New Bed
-          </button>
+            <InputGroup columns={3}>
+              <CustomInput
+                label="Hospital Pincode"
+                name="hospitalPincode"
+                type="dropdown"
+                value={section.hospitalPincode}
+                options={genderOptions}
+                onChange={(e) =>
+                  handleChange(
+                    sectionIndex,
+                    0,
+                    "hospitalPincode",
+                    e.target.value
+                  )
+                }
+              />
+              <CustomInput
+                label="Hospital State"
+                name="hospitalState"
+                type="text"
+                value={section.hospitalState}
+                onChange={(e) =>
+                  handleChange(sectionIndex, 0, "hospitalState", e.target.value)
+                }
+              />
+              <CustomInput
+                label="Hospital City"
+                name="hospitalCity"
+                type="text"
+                value={section.hospitalCity}
+                onChange={(e) =>
+                  handleChange(sectionIndex, 0, "hospitalCity", e.target.value)
+                }
+              />
+            </InputGroup>
 
+            <InputGroup columns={1}>
+              <CustomInput
+                label="ICU Name"
+                name="icuName"
+                type="text"
+                value={section.icuName}
+                onChange={(e) =>
+                  handleChange(sectionIndex, 0, "icuName", e.target.value)
+                }
+              />
+            </InputGroup>
+
+            <InputGroup columns={4}>
+              <CustomInput
+                label="Camera IP Address"
+                name="cameraIpAddress"
+                type="text"
+                value={section.cameraIpAddress}
+                onChange={(e) =>
+                  handleChange(
+                    sectionIndex,
+                    0,
+                    "cameraIpAddress",
+                    e.target.value
+                  )
+                }
+              />
+              <CustomInput
+                label="RTSP Port Number"
+                name="rtspPort"
+                type="text"
+                value={section.rtspPort}
+                onChange={(e) =>
+                  handleChange(sectionIndex, 0, "rtspPort", e.target.value)
+                }
+              />
+              <CustomInput
+                label="Username"
+                name="username"
+                type="text"
+                value={section.username}
+                onChange={(e) =>
+                  handleChange(sectionIndex, 0, "username", e.target.value)
+                }
+              />
+              <CustomInput
+                label="Password"
+                name="password"
+                type="text"
+                value={section.password}
+                onChange={(e) =>
+                  handleChange(sectionIndex, 0, "password", e.target.value)
+                }
+              />
+            </InputGroup>
+
+            {/* Bed Section */}
+            <h3>Bed Details</h3>
+            {section.beds.map((bed, bedIndex) => (
+              <InputGroup columns={3} key={bedIndex}>
+                <CustomInput
+                  label={`${bed.bedNumber}`}
+                  name={`bed-${bed.bedNumber}`}
+                  type="text"
+                  value={bed.monitorIP}
+                  onChange={(e) =>
+                    handleChange(
+                      sectionIndex,
+                      bedIndex,
+                      "monitorIP",
+                      e.target.value
+                    )
+                  }
+                />
+                <CustomInput
+                  label="Upper Body Preset Number"
+                  name={`upper-body-preset-${bed.bedNumber}`}
+                  type="text"
+                  value={bed.upperBodyPreset}
+                  onChange={(e) =>
+                    handleChange(
+                      sectionIndex,
+                      bedIndex,
+                      "upperBodyPreset",
+                      e.target.value
+                    )
+                  }
+                />
+                <CustomInput
+                  label="Lower Body Preset Number"
+                  name={`lower-body-preset-${bed.bedNumber}`}
+                  type="text"
+                  value={bed.lowerBodyPreset}
+                  onChange={(e) =>
+                    handleChange(
+                      sectionIndex,
+                      bedIndex,
+                      "lowerBodyPreset",
+                      e.target.value
+                    )
+                  }
+                />
+              </InputGroup>
+            ))}
+
+            <button
+              className="bg-orange-500 text-white font-bold py-2 px-4 rounded-lg focus:outline-none hover:bg-blue-600"
+              type="button"
+              onClick={() => handleAddBed(sectionIndex)}
+            >
+              + Add New Bed
+            </button>
+          </form>
           <button
             className="bg-orange-500 text-white font-bold py-2 px-4 rounded-lg focus:outline-none hover:bg-blue-600"
             type="submit"
           >
             + Add New ICU
           </button>
-        </form>
+        </>
       ))}
     </div>
   );
